92e01ffd4aba904ef4de854de637354654682519
Breno071 <brenonarde@gmail.com>
2021-10-05 18:28:22 -0300

    adiciona novos desafios

---
+++ Primeiros desafios com C#/Primeiros desafios com .NET/Primeiros desafios com .NET/QuantidadeNumerosPositivos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		int quantidadePositivos = 0;
+		for (int i = 0; i < 6; i++)
+		{
+			double valorInformadoPeloUsuario = double.Parse(Console.ReadLine());
+			if (valorInformadoPeloUsuario > 0)
+			{
+				quantidadePositivos++;
+			}
+		}
+		Console.WriteLine(quantidadePositivos + " valores positivos");
+	}
+}
---
+++ Primeiros desafios com C#/Primeiros desafios com .NET/Primeiros desafios com .NET/ValidacaoNotas.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float[] numeros = new float[2] { -1, -1 };
+
+        float media = 0, n = 0;
+
+        while (true)
+        {
+            n = float.Parse(Console.ReadLine());
+            while (n < 0 || n > 10)
+            {
+                Console.WriteLine("nota invalida");
+
+                n = float.Parse(Console.ReadLine());
+            }
+
+
+            if (numeros[0] != -1)
+            {
+                numeros[1] = n;
+                media = (numeros[0] + numeros[1]) / 2;
+                Console.WriteLine($"media = {media.ToString("F")}");
+                break;
+            }
+            else
+            {
+                numeros[0] = n;
+            }
+        }
+        Console.ReadKey();
+    }
+}
