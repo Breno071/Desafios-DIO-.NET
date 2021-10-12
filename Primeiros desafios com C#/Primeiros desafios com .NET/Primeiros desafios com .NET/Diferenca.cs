33b942c5b8454e0ea5153d77f2ba64870abe5eb5
Breno071 <brenonarde@gmail.com>
2021-10-12 16:24:00 -0300

    Adiciona novos desafios

---
+++ Primeiros desafios com C#/Primeiros desafios com .NET/Primeiros desafios com .NET/Diferenca.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primeiros_desafios_com_.NET
+{
+    class Diferenca
+    {
+        public static void Main(String[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int C = int.Parse(Console.ReadLine());
+            int D = int.Parse(Console.ReadLine());
+            Console.WriteLine("DIFERENCA = " + ((A * B) - (C * D)));
+            Console.ReadKey();
+        }
+        
+    }
+}
