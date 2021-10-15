193d08ce3ae9520be7a64583f08642c975ff8925
Breno071 <brenonarde@gmail.com>
2021-10-15 15:17:46 -0300

    Adiciona novos projetos

---
+++ Desafios intermediários C#/Desafios intermediários .NET/Desafios intermediários .NET/PreenchimentoDeVetor1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        int valorLido = int.Parse(Console.ReadLine());
+        int[] n = new int[10];
+        n[0] = valorLido;
+
+        for (int i = 1; i < n.Length; i++)
+        {
+            n[i] = n[i - 1] * 2;
+
+        }
+
+        for (int c = 0; c < n.Length; c++)
+        {
+            Console.WriteLine($"N[{c}] = {n[c]}");
+        }
+
+    }
+
+}
---
+++ Desafios intermediários C#/Desafios intermediários .NET/Desafios intermediários .NET/TrigoNoTabuleiro.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        ulong resultado = 0;
+        int qtdTeste = int.Parse(Console.ReadLine());
+        for (int i = 0; i < qtdTeste; i++)
+        {
+            ulong casas = ulong.Parse(Console.ReadLine());
+
+            if (casas > 1)
+            {
+                ulong[] tabuleiro = new ulong[casas];
+                tabuleiro[0] = 1;
+                resultado += tabuleiro[0];
+                for (int c = 1; c < tabuleiro.Length; c++)
+                {
+
+                    tabuleiro[c] = tabuleiro[c - 1] * 2;
+                    resultado += tabuleiro[c];
+                }
+                resultado = resultado / 12000;
+                Console.WriteLine($"{resultado} kg");
+                resultado = 0;
+            }
+            else
+            {
+                Console.WriteLine("0 kg");
+            }
+        }
+
+    }
+}
